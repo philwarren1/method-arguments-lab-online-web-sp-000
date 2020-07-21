83e4bae4e20e9fe383995cbc2f775eef08deb87d
Philip Warren <philwarren1@gmail.com>
2020-07-21 16:00:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,6 +5,6 @@ end
 name = "Dan"
 language = "Ember.js"
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name}, and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
